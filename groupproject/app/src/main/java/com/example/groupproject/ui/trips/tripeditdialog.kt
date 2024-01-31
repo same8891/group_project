30f7d80cb56cf548430fa3c7a8d757b4736c776e
same8891 <berine0401@gmail.com>
2024-01-31 13:55:25 -0600

    update trip data class

--- groupproject/app/src/main/java/com/example/groupproject/ui/trips/tripeditdialog.kt
+++ groupproject/app/src/main/java/com/example/groupproject/ui/trips/tripeditdialog.kt
@@ -54,158 +54,158 @@ import com.example.groupproject.ui.destination.RatingBar
 import com.example.groupproject.ui.destination.destinationCard
 import com.example.groupproject.ui.destination.reviewdialog
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun tripeditdialog(trip: Trip, AllDestination: List<String>, onDismiss: () -> Unit) {
-    var expanded by remember { mutableStateOf(false) }
-    val list = AllDestination.subtract(trip.destinationList).toList()
-    var select by remember { mutableStateOf(if (list.isNotEmpty()) list[0] else "") }
-    val icon = if (expanded) {
-        Icons.Outlined.KeyboardArrowUp
-    } else {
-        Icons.Outlined.KeyboardArrowDown
-    }
-        Dialog(
-            onDismissRequest = onDismiss,
-            properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false),
-        ) {
-            Column (modifier = Modifier.fillMaxWidth().background(Color.White)){
-                IconButton(
-                    onClick = { onDismiss() },
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .align(Alignment.End)
-                ) {
-                    Icon(imageVector = Icons.Default.Close, contentDescription = "Close")
-                }
-            }
-            Column(
-                modifier = Modifier
-//                    .padding(16.dp)
-                    .fillMaxWidth()
-                    .background(Color.White)
-            ) {
-                Row (horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth().background(Color.White)){
-                    ExposedDropdownMenuBox(
-                        expanded = expanded,
-                        onExpandedChange = {
-                            expanded = !expanded
-                        },
-                        modifier = Modifier
-                            .width(180.dp)
-                    ) {
-                        OutlinedTextField(
-                            value = select,
-                            onValueChange = { },
-                            placeholder = {
-                                Text(text = select)
-                            },
-                            readOnly = true,
-                            trailingIcon = {
-                                Icon(
-                                    imageVector = icon,
-                                    contentDescription = "Arrow"
-                                )
-                            },
-                            modifier = Modifier.menuAnchor()
-                        )
-                        ExposedDropdownMenu(
-                            expanded = expanded,
-                            onDismissRequest = { expanded = false },
-                            modifier = Modifier
-                                .height(300.dp),
-                        ) {
-                            list.forEach {
-                                DropdownMenuItem(
-                                    text = {
-                                        Text(text = it.toString())
-                                    },
-                                    onClick = {
-                                        select = it.toString()
-                                        expanded = false
-                                    }
-                                )
-                            }
-                        }
-
-                    }
-                    Button(
-                        onClick = {
-//                        Add it to destination list
-                        },
-                        modifier = Modifier.padding(start = 12.dp)
-                    ) {
-                        Text("Add")
-                    }
-
-                }
-                LazyColumn(modifier = Modifier.size(300.dp)) {
-                    items(trip.destinationList) { destination ->
-                        showitem(
-                            itemName = destination,
-                            onUpClick = { /* Handle up arrow click */ },
-                            onDownClick = { /* Handle down arrow click */ },
-                            onDeleteClick = { /* Handle delete button click */ }
-                        )
-                    }
-                }
-            }
-        }
-
-}
-@Preview(showBackground = true)
-@Composable
-fun preida() {
-    var trip = listOf("kyoto", "Taipei", "NewYork")
-    var alltrip = Trip(destinationList = listOf("kyoto", "Taipei", "NewYork", "paris"))
-    tripeditdialog(alltrip,trip){}
-//    showitem(itemName = "taipei", onUpClick = { }, onDownClick = {}, onDeleteClick = {})
-}
-@Composable
-fun showitem(itemName: String, onUpClick: () -> Unit, onDownClick: () -> Unit, onDeleteClick: () -> Unit) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-
-        Card(
-            modifier = Modifier
-                .weight(1f)
-                .padding(8.dp)
-        ) {
-            Row (verticalAlignment = Alignment.CenterVertically){
-                IconButton(
-                    onClick = { onUpClick() },
-                    modifier = Modifier.size(20.dp)
-                ) {
-                    Icon(imageVector = Icons.Default.ArrowUpward, contentDescription = "Up")
-                }
-
-                // Down arrow
-                IconButton(
-                    onClick = { onDownClick() },
-                    modifier = Modifier.size(20.dp)
-                ) {
-                    Icon(imageVector = Icons.Default.ArrowDownward, contentDescription = "Down")
-                }
-                IconButton(
-                    onClick = {onDeleteClick()},
-                    modifier = Modifier
-                        .size(20.dp)
-                        .size(40.dp)
-                ) {
-                    Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
-                }
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                ) {
-                    Text(text = itemName, fontSize = 30.sp)
-                }
-            }
-        }
-    }
-}
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun tripeditdialog(trip: Trip, AllDestination: List<String>, onDismiss: () -> Unit) {
+//    var expanded by remember { mutableStateOf(false) }
+//    val list = AllDestination.subtract(trip.destinationList).toList()
+//    var select by remember { mutableStateOf(if (list.isNotEmpty()) list[0] else "") }
+//    val icon = if (expanded) {
+//        Icons.Outlined.KeyboardArrowUp
+//    } else {
+//        Icons.Outlined.KeyboardArrowDown
+//    }
+//        Dialog(
+//            onDismissRequest = onDismiss,
+//            properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false),
+//        ) {
+//            Column (modifier = Modifier.fillMaxWidth().background(Color.White)){
+//                IconButton(
+//                    onClick = { onDismiss() },
+//                    modifier = Modifier
+//                        .padding(8.dp)
+//                        .align(Alignment.End)
+//                ) {
+//                    Icon(imageVector = Icons.Default.Close, contentDescription = "Close")
+//                }
+//            }
+//            Column(
+//                modifier = Modifier
+////                    .padding(16.dp)
+//                    .fillMaxWidth()
+//                    .background(Color.White)
+//            ) {
+//                Row (horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth().background(Color.White)){
+//                    ExposedDropdownMenuBox(
+//                        expanded = expanded,
+//                        onExpandedChange = {
+//                            expanded = !expanded
+//                        },
+//                        modifier = Modifier
+//                            .width(180.dp)
+//                    ) {
+//                        OutlinedTextField(
+//                            value = select,
+//                            onValueChange = { },
+//                            placeholder = {
+//                                Text(text = select)
+//                            },
+//                            readOnly = true,
+//                            trailingIcon = {
+//                                Icon(
+//                                    imageVector = icon,
+//                                    contentDescription = "Arrow"
+//                                )
+//                            },
+//                            modifier = Modifier.menuAnchor()
+//                        )
+//                        ExposedDropdownMenu(
+//                            expanded = expanded,
+//                            onDismissRequest = { expanded = false },
+//                            modifier = Modifier
+//                                .height(300.dp),
+//                        ) {
+//                            list.forEach {
+//                                DropdownMenuItem(
+//                                    text = {
+//                                        Text(text = it.toString())
+//                                    },
+//                                    onClick = {
+//                                        select = it.toString()
+//                                        expanded = false
+//                                    }
+//                                )
+//                            }
+//                        }
+//
+//                    }
+//                    Button(
+//                        onClick = {
+////                        Add it to destination list
+//                        },
+//                        modifier = Modifier.padding(start = 12.dp)
+//                    ) {
+//                        Text("Add")
+//                    }
+//
+//                }
+//                LazyColumn(modifier = Modifier.size(300.dp)) {
+//                    items(trip.destinationList) { destination ->
+//                        showitem(
+//                            itemName = destination,
+//                            onUpClick = { /* Handle up arrow click */ },
+//                            onDownClick = { /* Handle down arrow click */ },
+//                            onDeleteClick = { /* Handle delete button click */ }
+//                        )
+//                    }
+//                }
+//            }
+//        }
+//
+//}
+//@Preview(showBackground = true)
+//@Composable
+//fun preida() {
+//    var trip = listOf("kyoto", "Taipei", "NewYork")
+//    var alltrip = Trip(destinationList = listOf("kyoto", "Taipei", "NewYork", "paris"))
+//    tripeditdialog(alltrip,trip){}
+////    showitem(itemName = "taipei", onUpClick = { }, onDownClick = {}, onDeleteClick = {})
+//}
+//@Composable
+//fun showitem(itemName: String, onUpClick: () -> Unit, onDownClick: () -> Unit, onDeleteClick: () -> Unit) {
+//    Row(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(8.dp),
+//        verticalAlignment = Alignment.CenterVertically
+//    ) {
+//
+//        Card(
+//            modifier = Modifier
+//                .weight(1f)
+//                .padding(8.dp)
+//        ) {
+//            Row (verticalAlignment = Alignment.CenterVertically){
+//                IconButton(
+//                    onClick = { onUpClick() },
+//                    modifier = Modifier.size(20.dp)
+//                ) {
+//                    Icon(imageVector = Icons.Default.ArrowUpward, contentDescription = "Up")
+//                }
+//
+//                // Down arrow
+//                IconButton(
+//                    onClick = { onDownClick() },
+//                    modifier = Modifier.size(20.dp)
+//                ) {
+//                    Icon(imageVector = Icons.Default.ArrowDownward, contentDescription = "Down")
+//                }
+//                IconButton(
+//                    onClick = {onDeleteClick()},
+//                    modifier = Modifier
+//                        .size(20.dp)
+//                        .size(40.dp)
+//                ) {
+//                    Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
+//                }
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .padding(16.dp)
+//                ) {
+//                    Text(text = itemName, fontSize = 30.sp)
+//                }
+//            }
+//        }
+//    }
+//}
